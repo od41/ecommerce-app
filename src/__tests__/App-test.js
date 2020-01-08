f42f8af47752c8d75e6370541676f9acc34517bd
Odafe Aror <oaror@ipnxnigeria.net>
2020-01-08 22:24:40 +0100

    first commit

---
+++ src/__tests__/App-test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+import Timeline from '../Timeline'
+
+describe('App', () => {
+
+    it('passing test', () => {
+        expect(true).toBeTruthy();
+    })
+
+    it('failing test', () => {
+        expect(false).toBeFalsy();
+    })
+})
